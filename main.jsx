bf03430fa57eb3d4bb08ceb02763fcc2485cc398
Paolo <carabillopaolo@gmail.com>
2026-06-22 16:56:39 +0200

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import TWFMembershipCard from './TWFMembershipCard'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TWFMembershipCard />
-  </React.StrictMode>
-)
